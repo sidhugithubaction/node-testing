bd507ce30ddac5d06c031760c23d76687bea54cf
Sai Prasanth <141987317+sidhusaiprasanth@users.noreply.github.com>
2024-06-09 17:48:27 +0530

    xdvcsdvds
    dvsdvsdv

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/This is Prudhvi, Node Testing App/i);
+  const linkElement = screen.getByText(/This is Prasanth, Node Testing App/i);
   expect(linkElement).toBeInTheDocument();
 });
